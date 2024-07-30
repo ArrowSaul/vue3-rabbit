fa1ec1c67513515024be78b7438aba1813834300
wangshuo <2192595668@qq.com>
2024-07-30 15:07:50 +0800

    吸附导航

--- src/main.js
+++ src/main.js
@@ -6,10 +6,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-// import { getCategoryAPI } from './apis/testAPI'
-// getCategoryAPI().then(res => {
-//   console.log(res)
-// })
+
 const app = createApp(App)
 
 app.use(createPinia())
