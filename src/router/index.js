830c9461ff420746c8e568b5fd0d1b1a607d237d
wangshuo <2192595668@qq.com>
2024-07-30 13:04:54 +0800

    安装提示错误插件，导入样式图片

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,31 @@
+//createRouter: 创建router实例对象
+//createWebHistory: 创建history模式路由
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Login from '@/views/Login/Index.vue'
+import Layout from '@/views/Layout/Index.vue'
+import Home from '@/views/Home/Index.vue'
+import Category from '@/views/Category/Index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  //配置path和component之间的映射关系
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: Layout,
+      children: [
+        {
+          path: '',
+          component: Home
+        },
+        {
+          path: 'category',
+          component: Category
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/login',
+      component: Login
     }
   ]
 })
