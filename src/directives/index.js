698963a97b5ace99a1c4f93eb395cd47206cc74f
wangshuo <2192595668@qq.com>
2024-08-03 14:15:23 +0800

    图片懒加载优化

---
+++ src/directives/index.js
@@ -0,0 +1,23 @@
+import { useIntersectionObserver } from '@vueuse/core'
+export const lazyPlugin = {
+    install(app) {
+        app.directive('image-lazy', {
+            mounted(el, binding) {
+                //el:指令绑定元素 img
+                //binging: binding.value 指令等于号后面绑定的表达式的值 图片url
+                console.log(el, binding.value)
+                const { stop } = useIntersectionObserver(
+                    el,
+                    ([{ isIntersecting }]) => {
+                        console.log(isIntersecting)
+                        if (isIntersecting) {
+                            //进入视口区域
+                            el.src = binding.value
+                            stop()
+                        }
+                    },
+                )
+            }
+        })
+    }
+}
