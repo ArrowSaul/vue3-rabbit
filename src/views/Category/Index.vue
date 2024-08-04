b552a2c695e142a1463ba546a081c20a10a64700
wangshuo <2192595668@qq.com>
2024-08-04 18:38:44 +0800

    路由缓存

--- src/views/Category/Index.vue
+++ src/views/Category/Index.vue
@@ -1,20 +1,26 @@
 <script setup>
+import { onBeforeRouteUpdate } from "vue-router";
 import { useRoute } from "vue-router";
 import { ref } from "vue";
 import { onMounted } from "vue";
 import { getCategoryAPI } from "@/apis/category";
 import { getBannerAPI } from "@/apis/home";
-import GoodsItem from "../Home/components/GoodsItem.vue"
+import GoodsItem from "../Home/components/GoodsItem.vue";
 const categoryData = ref({});
 const route = useRoute();
-const getCategory = async () => {
+const getCategory = async (id = route.params.id) => {
   // 如何在setup中获取路由参数 useRoute() -> route 等价于this.$route
-  const res = await getCategoryAPI(route.params.id);
+  const res = await getCategoryAPI(id);
   categoryData.value = res.result;
 };
 onMounted(() => {
   getCategory();
 });
+//路由参数变化时，把分类数据接口重新发送
+onBeforeRouteUpdate((to) => {
+  getCategory(to.params.id);
+});
+
 const bannerList = ref([]);
 
 const getBanner = async () => {
@@ -46,7 +52,7 @@ onMounted(() => getBanner());
           </el-carousel-item>
         </el-carousel>
       </div>
-       <!-- 分类 -->
+      <!-- 分类 -->
       <div class="sub-list">
         <h3>全部分类</h3>
         <ul>
--- src/views/Layout/Index.vue
+++ src/views/Layout/Index.vue
@@ -1,11 +1,11 @@
 <script setup>
-import LayoutNav from './components/LayoutNav.vue'
-import LayoutHeader from './components/LayoutHeader.vue'
-import LayoutFooter from './components/LayoutFooter.vue'
-import LayoutFixed from './components/LayoutFixed.vue';
+import LayoutNav from "./components/LayoutNav.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
+import LayoutFooter from "./components/LayoutFooter.vue";
+import LayoutFixed from "./components/LayoutFixed.vue";
 //触发获取导航
-import { useCategoryStore } from '@/stores/category';
-import { onMounted } from 'vue';
+import { useCategoryStore } from "@/stores/category";
+import { onMounted } from "vue";
 const categoryStore = useCategoryStore();
 onMounted(() => {
   categoryStore.getCategory();
@@ -17,5 +17,6 @@ onMounted(() => {
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
+  <!-- <RouterView :key="$route.fullPath" /> -->
   <LayoutFooter />
 </template>
