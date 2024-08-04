0aa3e359ede0afa57c630326b3005bf9878daf51
wangshuo <2192595668@qq.com>
2024-08-04 18:00:08 +0800

    轮播图实现

--- src/views/Category/Index.vue
+++ src/views/Category/Index.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { useRoute } from "vue-router";
 import { ref } from "vue";
-import { onMounted } from 'vue';
+import { onMounted } from "vue";
 import { getCategoryAPI } from "@/apis/category";
+import { getBannerAPI } from "@/apis/home";
 const categoryData = ref({});
 const route = useRoute();
 const getCategory = async () => {
@@ -13,7 +14,17 @@ const getCategory = async () => {
 onMounted(() => {
   getCategory();
 });
+const bannerList = ref([]);
 
+const getBanner = async () => {
+  const res = await getBannerAPI({
+    distributionSite: "2",
+  });
+  console.log(res);
+  bannerList.value = res.result;
+};
+
+onMounted(() => getBanner());
 </script>
 
 <template>
@@ -25,6 +36,14 @@ onMounted(() => {
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
+      </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="" />
+          </el-carousel-item>
+        </el-carousel>
       </div>
     </div>
   </div>
@@ -106,6 +125,16 @@ onMounted(() => {
 
   .bread-container {
     padding: 25px 0;
+  }
+}
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
   }
 }
 </style>
